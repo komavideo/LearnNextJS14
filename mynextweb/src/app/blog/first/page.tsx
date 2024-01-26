de0e09198264bb08af5013f673180f420bd9bae3
xiaoma <xiaoma@Koma-MacBook-Air-2022.local>
2024-01-26 12:06:17 +0900

    20240126-120617-updated(LearnNextJS14).

---
+++ mynextweb/src/app/blog/first/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>First Blog Post</h1>
+        </div>
+    );
+}
+
---
+++ mynextweb/src/app/products/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+    return (
+        <div>
+            <h1>Products</h1>
+            <hr />
+            <h2>Product 1</h2>
+            <h2>Product 2</h2>
+            <h2>Product 3</h2>
+        </div>
+    );
+}
